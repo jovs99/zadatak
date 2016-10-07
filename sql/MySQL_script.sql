1190abceef0d7d0cad985f3b78895f7228c7106d
jbrestovac <jovana.brestovac@gmail.com>
2016-10-07 17:06:59 +0200

    Removed MySql_script

--- sql/MySQL_script.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `sequencesdb`.`sequence` (
-  `seqId` int(11) NOT NULL AUTO_INCREMENT,
-  `submitted_by` varchar(45) NOT NULL,
-  `purpose` varchar(45) NOT NULL,
-  `date_created` datetime NOT NULL,
-  PRIMARY KEY (`seqId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
